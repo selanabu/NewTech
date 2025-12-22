04482a5ad327e5c8b69e985b744826054235a773
Selan Abu Saleh <abusalehselan01@gmail.com>
2025-12-22 22:26:07 +0200

    until Ex2.5

---
+++ java/JS/solution1.js
@@ -0,0 +1,74 @@
+
+///// Ex1.1 - Yes or No
+function YesOrNo(bool){
+    if (bool === true){
+        return "Yes";
+    }else{
+        return "No";
+    }
+}
+
+console.log(YesOrNo(true));
+
+
+///// Ex2.1 - Sum of two lowest positive integers
+function SumOfLowestNumbers(numbers) {
+ let sortedNumbers = numbers.sort((a, b) => a - b);
+ return sortedNumbers[0]+sortedNumbers[1];
+}
+
+console.log(SumOfLowestNumbers([19, 5, 42, 2, 77])); // Output: 7
+
+
+/////Ex2.2 - One and Zero - Binary
+function BinaryArrayToNumber(arr) {
+   let k = 1;
+   let i = arr.length - 1;
+   let sum=0;
+
+    while(i>=0){
+        sum +=arr[i]*k;
+        k = k*2;
+        i--;
+    }   
+    return sum;
+}
+
+console.log(BinaryArrayToNumber([1,0,1,1])); 
+
+
+///// Ex2.3 - Find the Next Perfect Square
+function FindNextPerfectSquare(sq) {
+    let root = Math.sqrt(sq);
+    if (Number.isInteger(root)) {
+      return Math.pow(root + 1, 2);
+    } else {
+      return -1;
+    }
+}
+console.log(FindNextPerfectSquare(11));
+
+
+/////Ex2.4 - Unique
+function Uniq(arr) {
+    arr.sort((a, b) => a - b);
+    if (arr[0] !== arr[1]) {
+        return arr[0];
+    } else {
+        return arr[arr.length - 1];
+    }   
+
+}
+console.log(Uniq([ 1, 1, 1, 0.55, 1, 1 ]));
+
+/////Ex2.5 - Summation
+function Summation(num) {
+    let sum = num;
+    while (num > 0) {
+        num -= 1;
+        sum += num;
+    }
+    return sum;
+}
+console.log(Summation(8));
+
